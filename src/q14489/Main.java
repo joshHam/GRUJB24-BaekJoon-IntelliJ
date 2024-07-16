2dd74243f99bc822c78f990bb994fd8ebe04e0e2
joshHam <163796630+joshHam@users.noreply.github.com>
2024-07-16 17:17:24 +0900

    q14489

--- src/q14489/Main.java
+++ src/q14489/Main.java
@@ -1,4 +1,22 @@
 package q14489;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextInt();
+        long B = sc.nextInt();
+        long C = sc.nextInt();
+        sc.close();
+
+/*        System.out.println(A + B);
+        System.out.println(C);*/
+        if(A+B>=2*C){
+          System.out.println(A+B-2*C);
+        }else{
+            System.out.println(A+B);
+        }
+    }
 }
